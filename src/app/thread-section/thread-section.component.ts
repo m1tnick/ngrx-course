bef75c70b623f6de3abfbece795c3cf800f1de07
RODRIGUES FERREIRA Vanio (DIGIT-EXT) <Vanio.RODRIGUES-FERREIRA@ext.ec.europa.eu>
2018-01-10 16:41:49 +0100

    27: Implementing unread messages counter

--- src/app/thread-section/thread-section.component.ts
+++ src/app/thread-section/thread-section.component.ts
@@ -3,8 +3,10 @@ import {ThreadsService} from "../services/threads.service";
 import { Store } from '@ngrx/store';
 import { ApplicationState } from 'app/store/application-state';
 import { LoadUserThreadsAction } from 'app/store/actions';
+import { Observable } from 'rxjs/Observable';
+import { Thread } from '../../../shared/model/thread';
 
-
+import * as _ from 'lodash';
 @Component({
   selector: 'thread-section',
   templateUrl: './thread-section.component.html',
@@ -12,11 +14,33 @@ import { LoadUserThreadsAction } from 'app/store/actions';
 })
 export class ThreadSectionComponent implements OnInit {
 
+  userName$: Observable<string>;
+  unreadMessagesCounter$: Observable<number>;
+
   constructor(private threadsService: ThreadsService,
     private store: Store<ApplicationState>) {
-      store.subscribe(
-        state => console.log('thread section received state', state)
-      );
+
+      this.userName$ = store
+                      .skip(1)
+                      .map(this.mapStateToUserName);
+
+      this.unreadMessagesCounter$ =
+        store
+        .skip(1)                      
+        .map(this.mapStateToUnreadMessagesCounter);
+        
+  }
+
+  mapStateToUserName(state:ApplicationState):string {
+    return state.storeData.participants[state.uiState.userId].name;
+  }
+
+  mapStateToUnreadMessagesCounter(state: ApplicationState): number {
+
+    const currentUserId = state.uiState.userId;
+
+    return _.values<Thread>(state.storeData.threads)
+      .reduce((acc, thread) =>acc + thread.participants[currentUserId], 0);
   }
 
 
