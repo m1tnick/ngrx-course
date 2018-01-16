f0c6b6960c919f5fc8d048d850edd9043e71ab51
RODRIGUES FERREIRA Vanio (DIGIT-EXT) <Vanio.RODRIGUES-FERREIRA@ext.ec.europa.eu>
2018-01-16 17:18:34 +0100

    48 Implementation of the Change Selected User

--- src/app/store/reducers/uiStateReducer.ts
+++ src/app/store/reducers/uiStateReducer.ts
@@ -1,16 +1,26 @@
 import { INITIAL_UI_STATE, UiState } from "app/store/ui-state";
 import { Action } from "@ngrx/store";
-import { THREAD_SELECTED_ACTION } from "app/store/actions";
+import { THREAD_SELECTED_ACTION, SELECT_USER_ACTION, SelectUserAction, ThreadSelectedAction } from "app/store/actions";
 
 export function uiState(state: UiState = INITIAL_UI_STATE, action: Action) : UiState {
  
     switch(action.type) {
       case THREAD_SELECTED_ACTION:
         const newState = Object.assign({}, state);
-        newState.currentThreadId = action.payload;
+        const act = <ThreadSelectedAction> action;
+        newState.currentThreadId = act.payload;
         return newState;
+      case SELECT_USER_ACTION:
+        handleSelectUserAction(state, <SelectUserAction> action);
 
       default: 
         return state;
     }
+  }
+
+  function handleSelectUserAction(state: UiState = INITIAL_UI_STATE, action: SelectUserAction) {
+    const newUIState = Object.assign({}, state);
+    newUIState.userId = action.payload;
+
+    return newUIState;
   }
