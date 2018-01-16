0907e4fdd503896cf328c64c7ba234c276e7c6b0
RODRIGUES FERREIRA Vanio (DIGIT-EXT) <Vanio.RODRIGUES-FERREIRA@ext.ec.europa.eu>
2018-01-16 15:40:06 +0100

    43: Writing the reducing logic of select thread action

--- src/app/store/reducers/uiStateReducer.ts
+++ src/app/store/reducers/uiStateReducer.ts
@@ -1,9 +1,15 @@
 import { INITIAL_UI_STATE, UiState } from "app/store/ui-state";
 import { Action } from "@ngrx/store";
+import { THREAD_SELECTED_ACTION } from "app/store/actions";
 
 export function uiState(state: UiState = INITIAL_UI_STATE, action: Action) : UiState {
  
     switch(action.type) {
+      case THREAD_SELECTED_ACTION:
+        const newState = Object.assign({}, state);
+        newState.currentThreadId = action.payload;
+        return newState;
+
       default: 
         return state;
     }
