6e34a33a167b2bc11e077bb7ebfecbeabaab0f47
RODRIGUES FERREIRA Vanio (DIGIT-EXT) <Vanio.RODRIGUES-FERREIRA@ext.ec.europa.eu>
2018-01-17 15:16:08 +0100

    53: Scrolling the Message list to the bottom

--- src/app/message-list/message-list.component.ts
+++ src/app/message-list/message-list.component.ts
@@ -1,19 +1,46 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { SimpleChanges, Component, OnChanges, Input, ViewChild, ElementRef } from '@angular/core';
 import { MessageVM } from 'app/message-section/message.vm';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'message-list',
   templateUrl: './message-list.component.html',
   styleUrls: ['./message-list.component.css']
 })
-export class MessageListComponent implements OnInit {
+export class MessageListComponent implements OnChanges {
 
   @Input()
     messages: MessageVM[];
 
+  @ViewChild('list')
+  list: ElementRef;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
+    if(changes['messages']) {
+      let previousMessages = changes['messages'].previousValue;
+
+      if (!previousMessages) {
+        previousMessages= [];
+      }
+
+      const newMessages = changes['messages'].currentValue;
+
+      if(newMessages.length > previousMessages.length) {
+        setTimeout(() => {
+          this.scrollLastMessageIntoView();
+        });
+      }
+    }
+  }
+
+  scrollLastMessageIntoView() {
+    const items = this.list.nativeElement.querySelectorAll('li');
+    const lastItem: any = _.last(items);
+    if(lastItem) {
+      lastItem.scrollIntoView();
+    }
   }
 
 }
