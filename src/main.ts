f81a186d8cb6d510b8ac0c2e1d55b5675ae7ff7d
Vânio Ferreira <vanioferreira@MacBook-Pro-de-Vanio.local>
2018-01-17 21:28:34 +0100

    60 Finish implementing the refresh message HTTP service

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/skip';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/withLatestFrom';
+import 'rxjs/add/operator/filter';
 import { Observable } from 'rxjs/Observable';
 
 const debuggerOn = true;
